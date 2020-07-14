dcee8e3dfd32b0593b252c5b65228becde3fbe3e
Pravin Patel <patelpra337@gmail.com>
2020-07-13 21:02:38 -0500

    Changed class to videoPreviewLayer, added switch and provide access to plist

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,24 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		// TODO: get permission
-		
-		showCamera()
-		
-	}
-	
-	private func showCamera() {
-		performSegue(withIdentifier: "ShowCamera", sender: self)
-	}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // TODO: get permission
+        
+        requestVideoPermisson()
+        
+    }
+    
+    private func requestPermissionAndShowCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            requestVideoPermisson()
+            
+        case .restricted:
+            preconditionFailure("Video is disables, please review device restrictions.")
+            
+        case .denied:
+            preconditionFailure("Tell the user tye can't case the app without giving permissions via settings > Privacy > Video")
+            
+        case .authorized:
+            showCamera()
+            
+        @unknown default:
+            preconditionFailure("A new status code was added that we need to handle")
+        }
+    }
+    
+    private func requestVideoPermisson() {
+        AVCaptureDevice.requestAccess(for: .video) { isGranted in
+            guard isGranted else {
+                preconditionFailure("UI Tell the user to enable permissions for Video/Camers")
+            }
+            
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
+    
+    private func showCamera() {
+        performSegue(withIdentifier: "ShowCamera", sender: self)
+    }
 }
